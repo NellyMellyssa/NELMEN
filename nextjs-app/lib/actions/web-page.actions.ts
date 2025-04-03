f64e42b0fe84dc1f254e36aebe44aaf65608fef5
Mellyssa Nelly <mellyssanelly56@gmail.com>
2025-04-03 10:27:56 +0530

    add changed made

--- nextjs-app/lib/actions/web-page.actions.ts
+++ nextjs-app/lib/actions/web-page.actions.ts
@@ -8,6 +8,38 @@ import { formatError } from '@/lib/utils';
 import { WebPageInputSchema, WebPageUpdateSchema } from '../validator';
 import { z } from 'zod';
 
+// CREATE
+export async function createWebPage(data: z.infer<typeof WebPageInputSchema>) {
+  try {
+    const webPage = WebPageInputSchema.parse(data);
+    await connectToDatabase();
+    await WebPage.create(webPage);
+    revalidatePath('/admin/web-pages');
+    return {
+      success: true,
+      message: 'WebPage created successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// UPDATE
+export async function updateWebPage(data: z.infer<typeof WebPageUpdateSchema>) {
+  try {
+    const webPage = WebPageUpdateSchema.parse(data);
+    await connectToDatabase();
+    await WebPage.findByIdAndUpdate(webPage._id, webPage);
+    revalidatePath('/admin/web-pages');
+    return {
+      success: true,
+      message: 'WebPage updated successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
 // DELETE
 export async function deleteWebPage(id: string) {
   try {
@@ -42,36 +74,4 @@ export async function getWebPageBySlug(slug: string) {
   const webPage = await WebPage.findOne({ slug, isPublished: true });
   if (!webPage) throw new Error('WebPage not found');
   return JSON.parse(JSON.stringify(webPage)) as IWebPage;
-}
-
-// CREATE
-export async function createWebPage(data: z.infer<typeof WebPageInputSchema>) {
-  try {
-    const webPage = WebPageInputSchema.parse(data);
-    await connectToDatabase();
-    await WebPage.create(webPage);
-    revalidatePath('/admin/web-pages');
-    return {
-      success: true,
-      message: 'WebPage created successfully',
-    };
-  } catch (error) {
-    return { success: false, message: formatError(error) };
-  }
-}
-
-// UPDATE
-export async function updateWebPage(data: z.infer<typeof WebPageUpdateSchema>) {
-  try {
-    const webPage = WebPageUpdateSchema.parse(data);
-    await connectToDatabase();
-    await WebPage.findByIdAndUpdate(webPage._id, webPage);
-    revalidatePath('/admin/web-pages');
-    return {
-      success: true,
-      message: 'WebPage updated successfully',
-    };
-  } catch (error) {
-    return { success: false, message: formatError(error) };
-  }
 }
